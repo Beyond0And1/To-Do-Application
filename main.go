c56411462be4a9733b94f8142bb544576579777b
David Lasker <davidlasker1488@MacBook-Air-sl-David.local>
2025-08-11 14:20:04 +0300

    good

--- main.go
+++ main.go
@@ -1,36 +1,43 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-	"sync"
-)
+import "fmt"
 
-var wg sync.WaitGroup
-
-func main() {
-	fmt.Println("begin cpu:", runtime.NumCPU())
-	fmt.Println("begin gs:", runtime.NumGoroutine())
-	wg.Add(2)
-	go func() {
-		fmt.Println("hello from one")
-		wg.Done()
-	}()
-
-	go func() {
-		fmt.Println("hello frome two")
-		wg.Done()
-	}()
-	fmt.Println("mid cpu:", runtime.NumCPU())
-	fmt.Println("mid gs:", runtime.NumGoroutine())
-
-	wg.Wait()
-	fmt.Println("exit")
-	fmt.Println("end cpu:", runtime.NumCPU())
-	fmt.Println("end gs:", runtime.NumGoroutine())
-
+type Person struct {
+	first string
 }
 
-/*in addition to the main goroutine, launch two additional goroutines
-○ each additional goroutine should print something out
-● use waitgroups to make sure each goroutine finishes before your program exists*/
+func (p *Person) speak() {
+     fmt.Println("hello")
+}
+
+type human interface {
+	speak()
+}
+
+func saySomething(h human) {
+	h.speak()
+}
+func main() {
+
+	p1 := Person{
+		first: "David",
+	}
+
+	//saySomething(p1)
+	saySomething(&p1)
+
+	p1.speak()
+}
+
+/*create a type person struct
+● attach a method speak to type person using a pointer receiver
+○ *person
+● create a type human interface
+○ to implicitly implement the interface, a human must have the speak method
+● create func “saySomething”
+○ have it take in a human as a parameter
+○ have it call the speak method
+● show the following in your code
+○ you CAN pass a value of type *person into saySomething
+○ you CANNOT pass a value of type person into saySomething
+● here is a hint if you need some help*/
