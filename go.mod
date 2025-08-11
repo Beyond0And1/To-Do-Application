f2f98fc7c64d40bca469e3056250c7ad40d5f1d8
David Lasker <davidlasker1488@MacBook-Air-sl-David.local>
2025-08-11 13:07:52 +0300

    great

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/David88-HB/hellogo
+
+go 1.24.4
+
+require (
+	github.com/GoesToEleven/dog v0.0.0-20230428023317-90bef1c76cb9 // indirect
+	github.com/GoesToEleven/puppy v1.2.0 // indirect
+	golang.org/x/crypto v0.40.0 // indirect
+	golang.org/x/exp v0.0.0-20250718183923-645b1fa84792 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
 
 func main() {
-    fmt.Println("Hello, Go Modules world!")
+	fmt.Println("begin cpu:", runtime.NumCPU())
+	fmt.Println("begin gs:", runtime.NumGoroutine())
+	wg.Add(2)
+	go func() {
+		fmt.Println("hello from one")
+		wg.Done()
+	}()
+
+	go func() {
+		fmt.Println("hello frome two")
+		wg.Done()
+	}()
+	fmt.Println("mid cpu:", runtime.NumCPU())
+	fmt.Println("mid gs:", runtime.NumGoroutine())
+
+	wg.Wait()
+	fmt.Println("exit")
+	fmt.Println("end cpu:", runtime.NumCPU())
+	fmt.Println("end gs:", runtime.NumGoroutine())
+
 }
+
+/*in addition to the main goroutine, launch two additional goroutines
+○ each additional goroutine should print something out
+● use waitgroups to make sure each goroutine finishes before your program exists*/
